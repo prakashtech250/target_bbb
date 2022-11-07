64bf615d9f1e2c08fae0f06bd02a7408e68bb0b2
Prakash <83502915+prakashtech250@users.noreply.github.com>
2022-11-07 19:36:19 +0545

    Add files via upload

--- target.py
+++ target.py
@@ -10,6 +10,7 @@ from unidecode import unidecode
 import csv
 import os
 import argparse
+import random
 
 class targetApi:
 
@@ -20,9 +21,20 @@ class targetApi:
         self.folder = 'output'
         self.filename = os.path.join(self.folder, filename)
         self.link = link
-
-    def _requests(self, url):
-        headers = {
+        self.cookies = self.cookiesToDict('cookies/target_cookies.json')
+        self.user_agent_list = [
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) Gecko/20190101 Firefox/70.0',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0',
+        ]
+        self.headers = {
             'authority': 'redsky.target.com',
             'accept': 'application/json',
             'accept-language': 'en-US,en;q=0.6',
@@ -34,25 +46,32 @@ class targetApi:
             'sec-gpc': '1',
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
         }
-        cookies = {
-            'TealeafAkaSid': 'Da5AV3noQszxoVSX1BBvJ11HkNeruGEC',
-            'visitorId': '01836E4974590201ACA02C25A0200A09',
-            'sapphire': '1',
-            'UserLocation': '44601|27.700|85.310|BA|NP',
-            'usprivacy': '1YY-',
-            'fiatsCookie': 'DSI_794|DSN_Canton|DSZ_44720',
-            'ci_pixmgr': 'other',
-            'crl8.fpcuid': '6e16c00f-c7cd-4fa9-8f48-6daa78fbf2eb',
-            'egsSessionId': '0e8a035e-fe77-42d6-a67e-9d14c75531d4',
-            'accessToken': 'eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJiNmJhNDNjYS1hNTY2LTQzZTEtOTExNi0wOWYwNjJjNDI4MTUiLCJpc3MiOiJNSTYiLCJleHAiOjE2NjQ5ODQ0NDUsImlhdCI6MTY2NDg5ODA0NSwianRpIjoiVEdULmIyNDljZWZlNGFlZDQ0MDhiYmVlMGRkMzZjYTJlZTJkLWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6IjRkM2U3NjFhZTRhMWZjMGUwNjBiMTY1N2NjMjU1Zjg1NjMyMzU5OGRmYzlkOThjODY0OTViMTA2NGYyNzAwMTciLCJzY28iOiJlY29tLm5vbmUsb3BlbmlkIiwiY2xpIjoiZWNvbS13ZWItMS4wLjAiLCJhc2wiOiJMIn0.ZYz9I5k7yymC5-Jqq3If2K3bUtm7sIi70uKpvqDhHKHv1KfuB8-Qt1Q-UWwUIBdGJUwUnY5WRLy8sg7s-eZmo7t6h_oQGiYLmnnVZm6N2PIPtBHKO13B4T7KyMblO4KFqze7C9_L_DOgegHDL0Ao2e6ZjiueZxOxDzZP63R2j7C1T8QU2TBkTR0-ycij3I4Sk6rtmNuNXukJrsdIT09aPfZ3cBEyMDO49vUHUAfpNzhX7A26EfLv_Ce0zAPojNQdC-MPRtDdxEnq1kSf1NkyUIxnU4Ne3cuhZHOdoLNYZaXWrtsxH7v6MZBRzaWtYLaAya26W2DRwaKMycnb1eu7Ow',
-            'idToken': 'eyJhbGciOiJub25lIn0.eyJzdWIiOiJiNmJhNDNjYS1hNTY2LTQzZTEtOTExNi0wOWYwNjJjNDI4MTUiLCJpc3MiOiJNSTYiLCJleHAiOjE2NjQ5ODQ0NDUsImlhdCI6MTY2NDg5ODA0NSwiYXNzIjoiTCIsInN1dCI6IkciLCJjbGkiOiJlY29tLXdlYi0xLjAuMCIsInBybyI6eyJmbiI6bnVsbCwiZW0iOm51bGwsInBoIjpmYWxzZSwibGVkIjpudWxsLCJsdHkiOmZhbHNlfX0.',
-            'refreshToken': 'pbrDqasvmejsGhWzpM8sNMjgQ3xwRvWMc27tFIyxEO_HcHMc6CTYQavuMXfN1g2GsoKl6x5xzuK8fnvI1wZYsw',
-            'ffsession': '{%22sessionHash%22:%2210e9b3c1f09c1b1664002654784%22%2C%22prevPageName%22:%22home:%20bath:%20beach%20towels%22%2C%22prevPageType%22:%22level%203%22%2C%22prevPageUrl%22:%22https://www.target.com/c/beach-towels-bath-home/red/-/N-5xtv6?sortBy=bestselling&moveTo=product-list-grid%22%2C%22prevSearchTerm%22:%22non-search%22%2C%22sessionHit%22:18}',
-            '_mitata': 'MmE3YjVjMDgzZWU1OThkN2YzYTdiMTYzMTBhZDQzYmEwZDA3OGEzNWE3NjE1NmVlYmQ0MWNhMDZhMjkxOTM4Nw==_/@#/1664898277_/@#/cOaP4hamOE7aXwla_/@#/NjIwNTg4YWE2ZDNkMGVhNjJjZWQzMWFlNjAwOGY1Yzg2NzA1MDJjZWNmNmNjMDk1MWY1MWE3MTdjMDE0NTA3Mg==_/@#/000',
-        }
+
+    def cookiesToDict(self, filename):
+        print("[green][+][/green] Getting cookies from [u]target_cookies.json[/u]")
+        data = None
+        cookies = {}
+        try:
+            with open(filename,"r") as f:
+                data = json.load(f)
+        except Exception as e:
+            print("[red][-] [u]target_cookies.json[/u] not found in the path[/red]")
+            return None
+        
+        try:
+            for i in data:
+                cookies[i['name']] = i['value']
+        except Exception as e:
+            print("[red][-] Cookies are not in proper format[/red]")
+            return None
+
+        return cookies
+
+    def _requests(self, url):
+        self.headers['User-Agent'] = random.choice(self.user_agent_list)
         while True:
             try:
-                response = requests.get(url, headers=headers, cookies=cookies)
+                response = requests.get(url, headers=self.headers, cookies=self.cookies)
                 break
             except Exception as e:
                 print(f'[red][-][/red] Error: {e}')
@@ -193,6 +212,46 @@ class targetApi:
             distribution = result.get('rating').get('distribution')
         return review_count, average_rating, total_rating, quality, value, distribution
 
+    def check_features(self, feature, title, highlight_for_attr):
+        if feature in title.lower():
+            return 1
+        elif feature in highlight_for_attr.lower():
+            return 1
+        else:
+            return 0
+
+    def get_swatches(self,result, title, tcin):
+        try:
+            themes = result['parent']['variation_summary']['themes']
+        except:
+            themes = None
+        swatch = None
+        swatches = []
+        best_selling = None
+        if themes:
+            for theme in themes:
+                if title == theme['name'].lower():
+                    swatchList = theme['swatches']
+                    for sw in swatchList:
+                        swatchValue = sw['value']
+                        swatches.append(swatchValue)
+                        if tcin == sw['first_child']['tcin']:
+                            best_selling = swatchValue
+        if swatches:
+            swatch = ', '.join(swatches)
+        return swatch, best_selling
+
+    def get_pack(self, title):
+        pack = None
+        for i in range(1, 10):
+            pack1 = f'{i}-pack'
+            pack2 = f'{i}pk'
+            pack3 = f'{i} pack'
+            if pack1 in title.lower() or pack2 in title.lower() or pack3 in title.lower():
+                pack = i
+                break
+        return pack
+
     def get_products(self, params_url, start):
         main_url = 'https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v1?'
         cat_id, sort_by = self.get_param_value(params_url)
@@ -218,9 +277,18 @@ class targetApi:
         params['sort_by'] = sort_by.strip()
 
         url = main_url + urlencode(params)
-        response = self._requests(url)
-        data = json.loads(response.text).get('data').get('search')
-        results = data.get('products')
+        while True:
+            response = self._requests(url)
+            try:   
+                data = json.loads(response.text).get('data').get('search')
+                results = data.get('products')
+                break
+            except:
+                print(response.text)
+                for i in range(10):
+                    print(f'There is an error...Script will try again in {10-i}  ', end='\r')
+                    time.sleep(1)
+                self.cookies = self.cookiesToDict('cookies/target_cookies.json')
         total_results = data.get('search_response').get('typed_metadata').get('total_results')
         try:
             colorList = self.get_facet_list(data, 'd_color_all')
@@ -253,6 +321,7 @@ class targetApi:
             highlight_for_attr = highlight[1] + highlight[2] + highlight[3] + highlight[4] + highlight[5]
             attributes = self.get_attribute(highlight_for_attr)
             pattern_new = self.get_pattern_new(highlight_for_attr)
+            pattern_new = ', '.join(pattern_new)
             fabric_type = self.get_fabric_type(highlight_for_attr)
             title = result.get('item').get('product_description').get('title')
             try:
@@ -260,10 +329,18 @@ class targetApi:
             except:
                 pass
             product_type = self.get_product_type(title)
-            sale_price = result.get('price').get('formatted_current_price')
+            product_type = ', '.join(product_type)
+            sale_price = result.get('price').get('formatted_current_price').replace('$','')
             list_price = result.get('price').get('formatted_comparison_price')
             if list_price is None:
                 list_price = sale_price
+            else:
+                list_price = list_price.replace('$','')
+            try:
+                discount = (float(list_price) - float(sale_price)) / float(list_price)
+                discount_per = discount * 100
+            except:
+                discount_per = None
             try:
                 materials = self.get_description(result, "Material")
             except:
@@ -301,39 +378,18 @@ class targetApi:
             tcin = result.get('tcin')
             try:
                 ribbons = result.get('parent').get('item').get('ribbons')
+                ribbons = ', '.join(ribbons)
             except:
-                ribbons = []
-            available_colors = []
-            best_selling_color = ''
-            try:
-                colors = self.get_swatches(result, 'Color')
-                for color in colors:
-                    if tcin in color:
-                        best_selling_color = color[1]
-                    else:
-                        available_colors.append(color[1])
-            except:
-                pass
-            for ac in available_colors:
-                if ac in list(self.colors_frequency.keys()):
-                    self.colors_frequency[ac] += 1
-                else:
-                    self.colors_frequency[ac] = 1
-            available_sizes = []
-            best_selling_size = ''
-            try:
-                sizes = self.get_swatches(result, 'Size')
-                for size in sizes:
-                    if tcin in size:
-                        best_selling_size = size[1]
-                    else:
-                        available_sizes.append(size[1])
-            except:
-                pass
+                ribbons = None
+
+            available_color, best_selling_color = self.get_swatches(result, 'color', tcin)
+            available_size, best_selling_size = self.get_swatches(result, 'size', tcin)
+
             pieces = self.get_description(result, 'Pieces')
             piece1 = self.get_description(result, 'Piece 1')
             fabric_name = self.get_description(result, 'Fabric Name')
             textile_material = self.get_description(result, 'Textile Material')
+            dimensions = self.get_description(result, 'Dimensions')
             style = self.get_description(result,'Style')
             protective_qualities = self.get_description(result, 'Care and Cleaning')
             fabric_weight_type = self.get_description(result, 'Fabric Weight Type')
@@ -350,47 +406,61 @@ class targetApi:
             review_count, rating, rating_count, quality, value, distribution = self.get_ratings_and_reviews(tcin)
             crawling_time = self._time_now()
             data = {
-                'Crawling Time': crawling_time,
                 'Best Selling Rank': self.best_selling_rank,
-                'Product Url': product_url,
-                'Category': 'Bath Towels',
-                'Name': title,
+                'Title': title,
                 'Brand': brand,
-                'Material': material,
-                'Pattern': pattern,
                 'Highlights 1': highlight[1],
                 'Highlights 2': highlight[2],
                 'Highlights 3': highlight[3],
                 'Highlights 4': highlight[4],
                 'Highlights 5': highlight[5],
-                'Best Selling Size': best_selling_size,
-                'Available size': available_sizes,
+                'Size': available_size,
+                'Pack': self.get_pack(title),
                 'Best Selling color': best_selling_color,
-                'Available color': available_colors,
+                'Available color': available_color,
                 'Overall star rating': rating,
                 'Total Number Reviews': review_count,
-                'Value': value,
-                'Quality': quality,
-                'Total Star Ratings': rating_count,
-                'Star1 count': distribution['1'],
-                'Star2 count': distribution['2'],
-                'Star3 count': distribution['3'],
-                'Star4 count': distribution['4'],
-                'Star5 count': distribution['5'],
                 'List Price': list_price,
                 'Sale Price': sale_price,
+                'Discount %': discount_per,
                 'No of pieces': pieces,
-                'Piece 1': piece1,
+                'Dimensions': dimensions,
                 'Fabric Name': fabric_name,
                 'Textile_material': textile_material,
-                'Style': style,
-                'Protective Qualities': protective_qualities,
                 'Fabric_weight_type': fabric_weight_type,
                 'Ribbons': ribbons,
                 'Product Type': product_type,
-                'Attribute': attributes,
                 'Pattern New': pattern_new,
-                'Fabric Type': fabric_type,
+                'Cabana': self.check_features('cabana', title, highlight_for_attr),
+                'Sand': self.check_features('sand', title, highlight_for_attr),
+                'Pestemal': self.check_features('pestemal', title, highlight_for_attr),
+                'Jacquard': self.check_features('jacquard', title, highlight_for_attr),
+                'Stripe/Striped': self.check_features('stripe', title, highlight_for_attr),
+                'Printed': self.check_features('printed', title, highlight_for_attr),
+                'Solid': self.check_features('solid', title, highlight_for_attr),
+                'Tie Dye': self.check_features('tie dye', title, highlight_for_attr),
+                'Quick Dry': self.check_features('quick dry', title, highlight_for_attr),
+                'Oversized': self.check_features('oversize', title, highlight_for_attr),
+                'Floral/Flower': self.check_features('floral', title, highlight_for_attr) or self.check_features('flower', title, highlight_for_attr),
+                'Adult': self.check_features('adult', title, highlight_for_attr),
+                'Kids': self.check_features('kids', title, highlight_for_attr),
+                'Product Url': product_url,
+
+                # 'Material': material,
+                # 'Available size': available_sizes,
+                # 'Value': value,
+                # 'Quality': quality,
+                # 'Total Star Ratings': rating_count,
+                # 'Star1 count': distribution['1'],
+                # 'Star2 count': distribution['2'],
+                # 'Star3 count': distribution['3'],
+                # 'Star4 count': distribution['4'],
+                # 'Star5 count': distribution['5'],
+                # 'Piece 1': piece1,
+                # 'Style': style,
+                # 'Protective Qualities': protective_qualities,
+                # 'Attribute': attributes,
+                # 'Fabric Type': fabric_type,
             }
 
             self.save_data(data)
@@ -404,10 +474,10 @@ class targetApi:
     def main(self):
         start = 0
         more = True
-        # url = 'https://www.target.com/c/bath-towels-home/-/N-5xtv9?sortBy=newest&moveTo=product-list-grid'
-        link = self.link
+        # url = 'https://www.target.com/c/beach-towels-bath-home/red/-/N-5xtv6?sortBy=bestselling&moveTo=product-list-grid'
+        url = self.link
         while more:
-            more = self.get_products(link, start)
+            more = self.get_products(url, start)
             start += 24
         print(f'[green][+][/green] Task completed successfully.')
 
